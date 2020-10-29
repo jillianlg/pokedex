eaa0c9a0f8f679c293b792d2568b41e83887d6ef
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-29 16:25:48 -0700

    log API response - sucess

--- src/FetchPage.js
+++ src/FetchPage.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import request from 'superagent'
 
 export default class FetchPage extends Component {
+    componentDidMount = async () => {
+        const response = await request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex')
+        console.log(response.body.results);
+    }
+    
     render() {
         return (
             <div className="fetch-page">
