a32a0216f7140b82a025f6bf7f01e62ccef722d7
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-29 16:53:57 -0700

    add state and render to page

--- src/FetchPage.js
+++ src/FetchPage.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react'
 import request from 'superagent'
 
 export default class FetchPage extends Component {
+    state = {
+        quotes: []
+    }
+
+
     componentDidMount = async () => {
         const response = await request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex')
-        console.log(response.body.results);
+        
+        this.setState({ quotes: response.body.results })
     }
     
     render() {
         return (
             <div className="fetch-page">
-                I am the fetch page
+                {JSON.stringify(this.state.quotes)}
             </div>
         )
     }
 }
+
