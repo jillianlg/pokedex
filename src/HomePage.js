812d32dafe8eab341144ed2dff6d26dc709f72d1
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-11-02 14:28:59 -0800

    test merge updates

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,7 +4,7 @@ export default class HomePage extends Component {
     render() {
         return (
             <div className="Home">
-                {/* <img src="poke-image.jpg" alt="pokemon charactes"></img> */}
+                
             </div>
         )
     }
